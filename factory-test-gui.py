0d482ffc7af63135b97a7297266f963fa066851a
Glyn Hudson <glyn.hudson@gmail.com>
2022-10-19 14:06:20 +0100

    multi line GUI

--- factory-test-gui.py
+++ factory-test-gui.py
@@ -12,7 +12,14 @@ w, h = window.winfo_screenwidth(), window.winfo_screenheight()
 window.geometry("%dx%d+0+0" % (w, h))
 #window.geometry('300x200')
 
+class WritableStringVar(tk.StringVar):
+    def write(self, added_text):
+        new_text = self.get() + added_text
+        self.set(new_text)
 
+    def clear(self):
+        self.set("")
+        
 class CmdThread (threading.Thread):
    def __init__(self, command, textvar):
         threading.Thread.__init__(self)
@@ -24,13 +31,14 @@ class CmdThread (threading.Thread):
         while not proc.poll():
             data = proc.stdout.readline()
             if data:
-                print(data)
-                self.textvar.set(data)
+                print(data, file=textvar)
+                #textvar.set(textvar.get()[:-1])
             else:
                 break
 
 def emontxv4():	
-	thread = CmdThread(['./upload_and_test.sh'], text)
+	thread = CmdThread(['./upload_and_test.sh'], textvar)
+	#thread = CmdThread(['./test.sh'], textvar)
 	thread.start()
 	#messagebox.showinfo( "Test", "This is a test notification")
 
@@ -41,9 +49,12 @@ label.pack(ipadx=10, ipady=10)
 B = tk.Button(window, text ="emonTx V4", command = emontxv4)
 B.pack()
 
-text = tk.StringVar()
-label = tk.Label(textvariable=text)
+
+textvar = WritableStringVar(window)
+label = tk.Label(window, textvariable=textvar)
 label.pack(ipadx=10, ipady=10)
+
+
 
 
 
