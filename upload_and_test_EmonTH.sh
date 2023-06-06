82d8e000cdf5fc5cd7f8eb1b65a3aa55a2d93ecd
Glyn Hudson <glyn.hudson@gmail.com>
2023-06-06 01:39:14 +0100

    emonth upload

--- upload_and_test_EmonTH.sh
+++ upload_and_test_EmonTH.sh
@@ -1,40 +1,26 @@
 #!/usr/bin/bash
 
-exit 0
+uartprogrammer=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0
 
-if [ -L /dev/serial/by-id/XXXXISPPROGXXXX ] ; then
-    if [ -e /dev/serial/by-id/XXXISPPROGXXXXX ] ; then
-        echo "- Burning bootloader via ISP..."
-        /usr/bin/avrdude -p atmega328p -c usbasp -P usb -e -U efuse:w:0x05:m -U hfuse:w:0xD6:m -U lfuse:w:0xFF:m -U flash:w:/home/pi/factory-test/bootloaders/optiboot_atmega328.hex:i -Ulock:w:0x0f:m > /home/pi/factory-test/isp.log       
-        check=$(grep -e "Verify successful TODO" /home/pi/factory-test/isp.log)
-        if [ ! "$check" ] ; then
-            echo "- Bootloader upload: **FAIL** ..is ISP connected correctly?"
-        else
-            echo "- Bootloader upload: PASS" 
-            
-            if [ -L /dev//dev/serial/by-id/XXXUARTPPROGXXXXX ] ; then
-                if [ -e /dev//dev/serial/by-id/XXXUARTPPROGXXXXX ] ; then
-                    echo "- Uploading factory test firmware..."
-                    /usr/bin/avrdude -uV -c arduino -p ATMEGA328P -P /dev//dev/serial/by-id/XXXUARTPPROGXXXXX -b115200 -Uflash:w:/home/pi/factory-test/EmonTH_FactoryTest.ino.hex:i -l /home/pi/factory-test/avrdude.log
-                    check=$(grep -e "bytes of flash verified" /home/pi/factory-test/avrdude.log)
-                    if [ ! "$check" ] ; then
-                        echo "- Firmware upload: **FAIL**"
-                    else
-                        echo "- Firmware upload: PASS"
-                        echo "- Running function test..."
-                        python3 /home/pi/factory-test/test-emonth.py
-                    fi
-                else
-                    echo "- UART Programmer: MISSING"
-                fi
-            else
-                echo "- UART Programmer: MISSING"
-            fi
-        fi
-    else 
-        echo "- ISP Programmer: MISSING"
+# # Use newer version of avrdude (6.3-20190619) 
+# echo "- Setting fuses via ISP..."
+ /home/pi/arduino-1.8.19/hardware/tools/avr/bin/avrdude -C /home/pi/arduino-1.8.19/hardware/tools/avr/etc/avrdude.conf -uq -p atmega328p -c usbasp -P usb -e -U lock:w:0x3F:m -U efuse:w:0xFD:m -U hfuse:w:0xDE:m -Ulfuse:w:0xFF:m 
+
+# echo "- Burning bootloader via ISP..."
+ /home/pi/arduino-1.8.19/hardware/tools/avr/bin/avrdude -C /home/pi/arduino-1.8.19/hardware/tools/avr/etc/avrdude.conf -uq -p atmega328p -c usbasp -P usb -U flash:w:/home/pi/factory-test/bootloaders/optiboot_atmega328.hex:i -Ulock:w:0x0F:m 
+
+if [ -e $uartprogrammer ]; then
+    echo "- Uploading factory test firmware..."
+    /home/pi/arduino-1.8.19/hardware/tools/avr/bin/avrdude -C /home/pi/arduino-1.8.19/hardware/tools/avr/etc/avrdude.conf  -uq -c arduino -p ATMEGA328P -P $uartprogrammer -b115200 -Uflash:w:/home/pi/factory-test/testfw/EmonTH_FactoryTest.ino.hex:i -l /home/pi/factory-test/avrdude.log
+    check=$(grep -e "bytes of flash verified" /home/pi/factory-test/avrdude.log)
+    if [ ! "$check" ] ; then
+        echo "- Firmware upload: **FAIL**"
+    else
+        echo "- Firmware upload: PASS"
+        echo "- Running function test..."
+        python3 /home/pi/factory-test/test-emonth.py
     fi
 else
-    echo "- ISP Programmer: MISSING"
+    echo "- ERROR UART Programmer: MISSING"
 fi
 echo "End"
